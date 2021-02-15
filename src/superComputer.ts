d95128d389b68fa518a1c17bf5f30d4815e9d0e6
Amoz <amoz.pay@epitech.eu>
2021-02-15 13:44:55 +0100

    exo

--- src/superComputer.ts
+++ src/superComputer.ts
@@ -1,9 +1,9 @@
 enum symbols {
-    '+',
-    '-',
-    '/',
-    '*',
-    '%'
+    ADD = '+',
+    SUB = '-',
+    DIV = '/',
+    MUL = '*',
+    MOD = '%'
 }
 
 type Callback = {
@@ -15,31 +15,31 @@ function callback(err: Error, result?: undefined): Error;
 function callback(err: null, result: number): number;
 
 function callback(err: Error | null, result: any): Error | number {
-    if (err) {
-        console.log(err.message);
-        return err;
-    }
-    console.log(`Result: ${result}`);
-    return result;
+  if (err) {
+    console.log(err.message);
+    return err;
+  }
+  console.log(`Result: ${result}`);
+  return result;
 }
 
-function superComputer(nb1: number, sym: symbols, nb2: number, callback: Callback){
-    let result;
-    let err;
-    switch (sym) {
-        case symbols["%"]: try {result = nb1 % nb2} catch(e) {err = e}
-            break;
-        case symbols["+"]: try {result = nb1 + nb2} catch(e) {err = e}
-            break;
-        case symbols["-"]: try {result = nb1 - nb2} catch(e) {err = e}
-            break;
-        case symbols["*"]: try {result = nb1 * nb2} catch(e) {err = e}
-            break;
-        case symbols["/"]: try {result = nb1 / nb2} catch(e) {err = e}
-            break;
-        default: return "Wrong operator"
-    }
-    return callback(err, result)
+function superComputer(nb1: number, sym: symbols, nb2: number, callback: Callback) {
+  let result;
+  let err;
+  switch (sym) {
+    case symbols.MOD: try { result = nb1 % nb2; } catch (e) { err = e; }
+      break;
+    case symbols.ADD: try { result = nb1 + nb2; } catch (e) { err = e; }
+      break;
+    case symbols.SUB: try { result = nb1 - nb2; } catch (e) { err = e; }
+      break;
+    case symbols.MUL: try { result = nb1 * nb2; } catch (e) { err = e; }
+      break;
+    case symbols.DIV: try { result = nb1 / nb2; } catch (e) { err = e; }
+      break;
+    default: return 'Wrong operator';
+  }
+  return callback(err, result);
 }
 
-superComputer(100, symbols["/"], 0, callback)
+superComputer(100, symbols.ADD, 0, callback);
